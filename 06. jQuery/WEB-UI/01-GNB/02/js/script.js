70341bd8103feb5a477b2e8cabca51778584fa62
junyoung610 <junyoungkim610@gmail.com>
2024-10-04 10:47:03 +0900

    최종완성

---
+++ 06. jQuery/WEB-UI/01-GNB/02/js/script.js
@@ -0,0 +1,32 @@
+$(function () {
+    const $window = $(window);
+    const $header = $("header");
+    const $menu = $(".gnb > li");
+    const $submenu = $(".submenu");
+    const duration = 300;
+
+    // GNB
+    $menu.on("mouseenter", function () {
+        $(this).addClass("on");
+        $header.addClass("active");
+        $submenu.stop().slideDown(duration);
+    });
+    $menu.on("mouseleave", function () {
+        $(this).removeClass("on");
+        $header.removeClass("active");
+        $submenu.stop().slideUp(duration);
+    });
+
+    // 마우스 휠을 조작했을 때
+    $window.on("wheel", function (e) {
+        // console.log(e);
+
+        if (e.originalEvent.wheelDelta > 0) {
+            // 휠을 올렸을 때
+            $header.removeClass("hide");
+        } else {
+            // 휠을 내렸을 때
+            $header.addClass("hide");
+        }
+    });
+});
