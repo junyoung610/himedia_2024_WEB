c09b26e56694860afb2c9bd7402353b7ee16444f
junyoung610 <junyoungkim610@gmail.com>
2024-11-04 13:41:06 +0900

    20241104 자료백업

--- 06. jQuery/WEB-UI/11-scrollTrigger/실습예제-agency/js/script.js
+++ 06. jQuery/WEB-UI/11-scrollTrigger/실습예제-agency/js/script.js
@@ -33,10 +33,15 @@ $(function () {
         "-=0.4"
     );
 
-    visualTL.from(".util", {
-        y: -100,
-        autoAlpha: 0,
-    });
+    visualTL.from(
+        ".util",
+        {
+            y: -100,
+            autoAlpha: 0,
+            delay: 1,
+        },
+        0
+    );
 
     visualTL.from(".visual-title h2 strong", {
         y: 100,
@@ -60,9 +65,6 @@ $(function () {
         "-=0.4"
     );
 
-    // 1-1. 공통의 요소에 같은 애니메이션 정의
-    const secTitle = gsap.utils.toArray(".sec-title");
-
     // 2. about 영역 애니메이션
     const abootTL = gsap.timeline({
         scrollTrigger: {
@@ -73,7 +75,7 @@ $(function () {
             end: "bottom 30%",
 
             // play none pause resume reset restart reverse
-            toggleAction: "play none reverse none", // onEnter, onLeave, onEnterBack, onLeaveBack
+            toggleActions: "play none reverse none", // onEnter, onLeave, onEnterBack, onLeaveBack
             onEnter: () => video.pause(),
         },
     });
@@ -91,9 +93,9 @@ $(function () {
     const teamTL = gsap.timeline({
         scrollTrigger: {
             trigger: ".team",
-            markers: true,
+            // markers: true,
             start: "top 10%",
-            end: "bottom 0%",
+            end: "bottom+=2000 0%",
             pin: true,
             // scrub: true,
             scrub: 1, // 사용자의 스크롤바를 따라잡기까지 1초
@@ -103,10 +105,48 @@ $(function () {
     teamTL.from(".team-list li figure", { autoAlpha: 0, rotation: 30, y: -100, stagger: 0.2 }, "<");
     teamTL.from(".team-list li dl", { autoAlpha: 0, y: -50, stagger: 0.2 }, "<");
 
+    // 4. work 영역 애니메이션
+    const workImg = gsap.utils.toArray(".work-con figure img");
+    console.log(workImg);
+
+    workImg.forEach((item, index) => {
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: item,
+                start: "top 50%",
+                end: "bottom 0%",
+                markers: true,
+            },
+        }).from(item, { x: -50, autoAlpha: 0, delay: index * 0.2 });
+    });
+    // 스크롤바가 상단으로 이동하면 동영상 다시 재생시키기
+    $(window).on("scroll", function () {
+        const scrollTop = $(this).scrollTop();
+
+        if (scrollTop === 0) video.play();
+    });
+
     // css 변경도 가능함
     teamTL.to(".team", { backgroundColor: "#f59", duration: 3 });
 
     teamTL.from(".btn-with", { autoAlpha: 0, y: -50 }, "-=0.3");
-
     teamTL.to(".fake-delay", { x: 1, duration: 10 });
+
+    // 1-1. 공통의 요소에 같은 애니메이션 정의
+    const secTitle = $(".sec-title");
+    console.log(secTitle);
+
+    // forEach()
+    secTitle.forEach((index, item) => {
+        gsap.from(item, {
+            y: 50,
+            autoAlpha: 0,
+
+            scrollTrigger: {
+                trigger: item,
+                // markers: true,
+                start: "top 50%",
+            },
+        });
+    });
 });
